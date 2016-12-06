84abccfed58dac83759dc866de2cd93d26e731c5
walston <nathanielwalston@gmail.com>
2016-12-05 16:41:21 -0800

    Connect to the database and query for image files

--- main.js
+++ main.js
@@ -7,9 +7,24 @@ const sql = require('sqlite3');
 
 const dbFile = path.join(process.env.RECOVERY_PATH, 'Manifest.db');
 const db = new sql.Database(dbFile, (err, res) => {
-  if (err) {
-    console.warn(`Error reading ${dbFile}`);
+  errHandler(err);
+});
+
+var query = [
+  'SELECT *',
+  'FROM Files',
+  'WHERE domain="CameraRollDomain"',
+  'AND relativePath like "Media/DCIM%"',
+  ';'
+].join(' ');
+db.each(query, [], function(err, results) {
+  errHandler(err);
+  console.log(results);
+})
+
+function errHandler (error) {
+  if (error) {
     console.warn(err);
     process.exit(2);
   }
-})
+}
