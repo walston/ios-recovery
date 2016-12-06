cdb50accc5196f3c36df96fc35e7c1e9609003ce
walston <nathanielwalston@gmail.com>
2016-12-05 16:28:27 -0800

    Using sqlite3 cuz thats what iOS uses

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
 require('dotenv').config();
+if (!process.env.RECOVERY_PATH) {
+  process.exit(1);
+}
+const path = require('path');
+const sql = require('sqlite3');
 
-console.log(process.env);
+const dbFile = path.join(process.env.RECOVERY_PATH, 'Manifest.db');
+const db = new sql.Database(dbFile, (err, res) => {
+  if (err) {
+    console.warn(`Error reading ${dbFile}`);
+    console.warn(err);
+    process.exit(2);
+  }
+})
