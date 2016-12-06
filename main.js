51c61aa04a6996dce162786c09db2c1696b4f717
walston <nathanielwalston@gmail.com>
2016-12-06 09:38:25 -0800

    Just create a webserver and serve up images that way

--- main.js
+++ main.js
@@ -3,12 +3,14 @@ if (!process.env.RECOVERY_PATH) {
   process.exit(1);
 }
 const path = require('path');
-const fs = require('fs');
 const sql = require('sqlite3');
+const express = require('express');
+
+const app = express();
 
 const dbFile = path.join(process.env.RECOVERY_PATH, 'Manifest.db');
 const db = new sql.Database(dbFile, (err, res) => {
-  errHandler(err);
+  if (err) {console.warn('Failed to connect to DB'); console.warn(err); }
 });
 
 var query = [
@@ -18,20 +20,42 @@ var query = [
   'AND relativePath like "Media/DCIM%"',
   ';'
 ].join(' ');
-db.each(query, [], function(err, result) {
-  errHandler(err);
-  writeFile(result);
+
+
+app.engine('pug', require('pug').__express);
+
+app.all('/', (req, res) => {
+  var q = new Promise(function(resolve, reject) {
+    var rows = [];
+    db.each(query, [],
+      function queryEach(err, result) {
+        if (err) {console.warn('Failed to Query'); console.warn(err); }
+        else rows.push(result);
+      },
+      function queryComplete(err, affectedRows) {
+        console.log('rows.length: ' + rows.length)
+        if (err) reject('Error on completion');
+        else resolve(rows);
+      }
+    );
+
+  });
+  q.then(function queryCompleted(rows) {
+    console.log(rows)
+    res.render('index.pug', {images: rows}, (err, html) => {
+      if (err) {
+        console.warn(err);
+      }
+      else {
+        res.send(html);
+      }
+    });
+  }).catch( function queryFailed(err) {
+    console.log(err)
+    res.status(500).send(err)
+  });
+});
+
+app.listen('8080', () => {
+  console.log('Listening on port 8080');
 })
-
-function errHandler (error) {
-  if (error) {
-    console.warn(err);
-    process.exit(2);
-  }
-}
-
-function writeFile(fileInfo) {
-  var pwd = process.env.WRITE_PATH || process.env.PWD;
-  var writePath = path.join(pwd, fileInfo.relativePath);
-  console.log(writePath);
-}
